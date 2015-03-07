77bd5c8f32de6596c7aa3f55c2e98ef3da88b661
darkman66 <agaihubi@gmail.com>
2015-03-07 12:59:14 +0800

    models for table definitions

--- langcodes/models.py
+++ langcodes/models.py
@@ -13,3 +13,61 @@ language = Table('language', metadata,
     Column('preferred', Text),
     Column('macrolang', Text)
 )
+
+
+extlang = Table('extlang', metadata,
+    Column('subtag', Text, primary_key = True),
+    Column('prefixes', Text)
+)
+language_name = Table('language_name',metadata,
+    Column('subtag', Text, primary_key = True),
+    Column('language', Text),
+    Column('name', Text),
+    Column('entry_order', Integer)
+)
+
+nonstandard = Table('nonstandard', metadata,
+    Column('tag', Text, primary_key = True),
+    Column('description', Text),
+    Column('preferred', Text),
+    Column('is_macro', Integer)
+)
+
+nonstandard_region = Table('nonstandard_region', metadata,
+    Column('subtag',Text, primary_key = True),
+    Column('preferred', Text)
+)
+
+region = Table('region', metadata,
+    Column('subtag', Text, primary_key = True),
+    Column('deprecated', Integer),
+    Column('preferred', Text)
+)
+
+region_name = Table('region_name', metadata,
+    Column('subtag', Text, primary_key = True),
+    Column('language', Text),
+    Column('name', Text),
+    Column('entry_order', Integer)
+)
+
+# we have no useful information about scripts except their name
+script_name = Table('script_name', metadata,
+    Column('subtag',Text, primary_key = True),
+    Column('language', Text),
+    Column('name', Text),
+    Column('entry_order', Integer)
+)
+
+# was there a reason variants saved their comments?
+variant = Table('variant', metadata,
+    Column('subtag',Text, primary_key = True),
+    Column('prefixes', Text)
+)
+
+variant_name = Table('variant_name', metadata,
+    Column('subtag',Text, primary_key = True),
+    Column('language', Text),
+    Column('name', Text),
+    Column('entry_order', Integer)
+)
